3ffb2859ac478300dd71fd5e8f11624fa5b537f5
Simon Crabtree <limajs@simoncrabtree.net>
2013-04-10 10:18:07 +0100

    Revert "Changed app name to a more generic 'app'"
    This reverts commit 47343ded33dad2fd2d966f1e9fce7422fb778757.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', [])
+angular.module('wispaApp', [])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app')
+angular.module('wispaApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
